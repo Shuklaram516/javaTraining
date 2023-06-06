269a9a47a1b31d59608bf95516d459ab8f189a22
Shuklaram516 <shuklaram516@gmail.com>
2023-06-06 15:54:57 +0530

    vehicle rental system

---
+++ Vehicle/src/com/rgt/vehiclerental/Car.java
@@ -0,0 +1,13 @@
+// Car.java
+package com.rgt.vehiclerental;
+
+public class Car extends Vehicle {
+    private int numberOfSeats;
+
+    public Car(String licensePlate, String make, String model, int numberOfSeats) {
+        super(licensePlate, make, model);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    // Car-specific properties and methods
+}
---
+++ Vehicle/src/com/rgt/vehiclerental/Customer.java
@@ -0,0 +1,16 @@
+// Customer.java
+package com.rgt.vehiclerental;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String id;
+
+    public Customer(String firstName, String lastName, String id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    // Getters and setters
+}
